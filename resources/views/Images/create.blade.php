ff3eaa63301b98c37818d909801fea1bc439a42c
omarIbrahim2 <rady1753@gmail.com>
2022-11-20 18:51:33 +0200

    Base

---
+++ resources/views/Images/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layout')
+
+
+
+
+
+@section('section')
+
+
+<div class="container px-4 py-5">
+
+
+    <div class="card shadow-sm">
+        
+       <div class="card-body">
+         <div class="d-flex justify-content-center flex-column">
+            <h3>Upload Image</h3>
+            <form action="">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Image Name</label>
+                <input type="text" class="form-control" name="name" id="exampleFormControlInput1" placeholder="name@example.com">
+                
+              </div>
+        
+              <input class="btn btn-secondary mt-3" value="Add" type="bottom">
+            </form>     
+        </div> 
+       
+           
+    
+     </div> 
+
+       
+    </div>
+    </div>
+    
+@endsection
